ed49d35d0d1fdafc913e3c3166c15b2214e0d3f5
David Sociolla <david.aji@sociolla.com>
2019-06-18 06:13:41 +0700

    template input data

--- resources/views/admin/transaction/add.blade.php
+++ resources/views/admin/transaction/add.blade.php
@@ -9,27 +9,74 @@
 
     <div class="box-body">
         <form>
-            <div class="col-md-6">
-                <div class="form-group has-feedback {{ $errors->first('description')? "has-error":null }}">
-                    <textarea class="form-control" name="description" placeholder="Deskripsi Lengkap" rows="5"></textarea> <span class="form-control-feedback fa fa-edit"></span>
-                    <span class="help-block">{{ $errors->first('description') }}</span>
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <div class="form-group has-feedback {{ $errors->first('description')? "has-error":null }}">
+                        <textarea class="form-control" name="description" placeholder="Deskripsi Lengkap" rows="5"></textarea> <span class="form-control-feedback fa fa-edit"></span>
+                        <span class="help-block">{{ $errors->first('description') }}</span>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-group has-feedback {{ $errors->first('code')? "has-error":null }}">
+                        <input type="text" class="form-control" name="code" placeholder="Code" />
+                        <span class="help-block">{{ $errors->first('code') }}</span>
+                    </div>
+
+                    <div class="form-group has-feedback {{ $errors->first('rate_euro')? "has-error":null }}">
+                        <input type="text" class="form-control" name="rate_euro" placeholder="Rate Euro" />
+                        <span class="help-block">{{ $errors->first('rate_euro') }}</span>
+                    </div>
+
+                    <div class="form-group has-feedback {{ $errors->first('date_paid')? "has-error":null }}">
+                        <input type="date" class="form-control" name="date_paid" placeholder="Date Paid" />
+                        <span class="help-block">{{ $errors->first('date_paid') }}</span>
+                    </div>
                 </div>
             </div>
 
-            <div class="col-md-6">
-                <div class="form-group has-feedback {{ $errors->first('code')? "has-error":null }}">
-                    <input type="text" class="form-control" name="code" placeholder="Code" />
-                    <span class="help-block">{{ $errors->first('code') }}</span>
-                </div>
+            <div class="col-md-12">
+                <div class="col-md-10 col-md-offset-1">
+                <div class="transaction-form">
+                    <div class="form-group row">
+                        <label for="category" class="col-sm-1 col-form-label">Category</label>
+                        <div class="col-sm-2">
+                            <select class="form-control" name="type">
+                                <option value="income">Income</option>
+                                <option value="expense">Expense</option>
+                            </select>
+                        </div>
+                    </div>
 
-                <div class="form-group has-feedback {{ $errors->first('rate_euro')? "has-error":null }}">
-                    <input type="text" class="form-control" name="rate_euro" placeholder="Rate Euro" />
-                    <span class="help-block">{{ $errors->first('rate_euro') }}</span>
+                    <div class="form-group row">
+                        <label for="category" class="col-sm-1 col-form-label"></label>
+                        <div class="col-sm-10">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Nama Transaksi</th>
+                                        <th>Nominal (IDR)</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody class="fields">
+                                    <tr>
+                                        <td>
+                                            <input class="form-control" type="text" name="name[]">
+                                        </td>
+                                        <td>
+                                            <input class="form-control" type="text" name="amount_id[]">
+                                        </td>
+                                        <td>
+                                            <a href="#" class="fa fa-plus btn-add"></a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <button class="btn btn-primary btn-flat pull-right btn-add-transaction">Tambah</button>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="form-group has-feedback {{ $errors->first('date_paid')? "has-error":null }}">
-                    <input type="text" class="form-control" name="date_paid" placeholder="Date Paid" />
-                    <span class="help-block">{{ $errors->first('date_paid') }}</span>
                 </div>
             </div>
 
@@ -40,4 +87,19 @@
         </form>
     </div>
 </div>
+@stop
+
+@section('custom-script')
+<script>
+    $(".fields").on("click", ".btn-add", function() {
+        let template = `<tr> <td> <input class="form-control" type="text" name="name[]"> </td> <td> <input class="form-control" type="text" name="amount_id[]"> </td> <td> <a href="#" class="fa fa-plus btn-add"></a> </td> </tr>`;
+
+        $(this).remove();
+        $(".fields").append(template);
+    });
+
+    $(".btn-add-transaction").click(function() {
+
+    });
+</script>
 @stop
